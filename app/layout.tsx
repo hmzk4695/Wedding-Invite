e0294344be0ad0d6e6534c1a31b736dbe56a1620
hmzk4695 <hmzk4695@gmail.com>
2026-08-24 22:02:37 +0200

    Thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/thumbnail-nikkah.jpg", // Or a preview image of your invitation
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 630,
       },
     ],
     locale: "en_US",
